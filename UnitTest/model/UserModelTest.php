558114f7c2ae5c862c4db3c906ba5bdf0acd2240
yangqingxian <qingxianyang@gmail.com>
2023-02-23 01:15:02 -0800

    开始编写单元测试

---
+++ UnitTest/model/UserModelTest.php
@@ -0,0 +1,8 @@
+<?php
+require_once dirname(dirname(__DIR__)).DIRECTORY_SEPARATOR."vendor".DIRECTORY_SEPARATOR."autoload.php";
+
+use PHPUnit\Framework\TestCase;
+
+class UserModelTest extends TestCase{
+
+}

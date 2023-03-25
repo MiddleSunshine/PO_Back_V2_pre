ed724beee991c20b8225f954d4db3d608c78881e
YangQingXian <qingxianyang@gmail.com>
2023-03-25 20:44:00 +0800

    设置LastUpdateTime

--- model/UserReleatedTable/class.NodeModel.php
+++ model/UserReleatedTable/class.NodeModel.php
@@ -3,17 +3,18 @@
 class NodeModel extends BaseModel{
     public $ID;
     public $AddTime;
-    public $LsatUpdateTime;
+    public $LastUpdateTime;
     public $Name;
     public $Type;
     public $LocalFilePath;
     public function updateNode($newNode){
-        $historyNodeLastUpdateTimestamp=strtotime($this->LsatUpdateTime);
+        $historyNodeLastUpdateTimestamp=strtotime($this->LastUpdateTime);
         $lastUpdateTimestamp=strtotime($newNode['LastUpdateTime']);
         // 如果数据库的数据更新，那么不更新
-        if ($historyNodeLastUpdateTimestamp>=$lastUpdateTimestamp){
+        if ($historyNodeLastUpdateTimestamp>$lastUpdateTimestamp){
             return false;
         }
+        $newNode['LastUpdateTime']=date("Y-m-d H:i:s");
         return $this->updateData(sprintf("ID=%d",$newNode['ID']),$newNode);
     }
 
